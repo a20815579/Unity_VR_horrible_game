c3936b016ee5264e90631bfbf076434b9a651bb1
a20815579 <48749915+a20815579@users.noreply.github.com>
2020-12-20 18:22:08 +0800

    20 18:22 744

---
+++ Assets/Scripts/StageManagers/Stage_5_Manager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage_5_Manager : StageManager
+{
+    public FoIn_vanish[] foIn_Vanish;
+    // Start is called before the first frame update
+    public override void SetupEvents()
+    {
+
+        /* add event-triggered functions to delegate list */
+        AddEvent(0, FoInVanish1); //click foIn -> Vanish
+        AddEvent(1, FoInVanish2); //click foIn -> Vanish
+        
+    }
+    void FoInVanish1()
+    {
+        foIn_Vanish[0].Vanish();
+    }
+    void FoInVanish2()
+    {
+        foIn_Vanish[1].Vanish();
+    }
+}
---
+++ Assets/Scripts/candle_lightup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class candle_lightup : MonoBehaviour
+{
+    public Light mylight;
+    public void candle_lightup_func()
+    {
+        mylight.enabled = true;
+        Debug.Log("light");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("light_unable");
+        mylight.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
