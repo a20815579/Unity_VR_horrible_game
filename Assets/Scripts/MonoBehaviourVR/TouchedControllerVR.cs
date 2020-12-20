a1329f50fdc4b57691f91faea031bb479c069070
rickywu0421 <rickywu0421@gmail.com>
2020-12-20 15:24:31 +0800

    add stage4 scene

--- Assets/Scripts/MonoBehaviourVR/TouchedControllerVR.cs
+++ Assets/Scripts/MonoBehaviourVR/TouchedControllerVR.cs
@@ -8,6 +8,8 @@ public class TouchedControllerVR : MonoBehaviour
     private InputActions inputActions;
     [SerializeField]
     int _id;
+    [SerializeField]
+    AudioSource audioSource;
 
     void Awake()
     {
@@ -16,10 +18,16 @@ public class TouchedControllerVR : MonoBehaviour
         inputActions.id = _id;
     }
 
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();    
+    }
+
     // trigger
     public void ResponseOnInput()
     {
         Debug.Log("ResponseOnInput");
+        audioSource.Play();
         inputActions.ResponseOnInput();
     }
 }
