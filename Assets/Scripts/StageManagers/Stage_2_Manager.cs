3c8fe9762735b64ef8d0f5dbbfca55f8dadd6995
rickywu0421 <rickywu0421@gmail.com>
2020-12-20 14:16:15 +0800

    update state2 manager

--- Assets/Scripts/StageManagers/Stage_2_Manager.cs
+++ Assets/Scripts/StageManagers/Stage_2_Manager.cs
@@ -14,24 +14,18 @@ public class Stage_2_Manager : StageManager
         //AddEvent(2, ShowPlayerUIMessage); //2:click window -> show message
         //AddEvent(3, TransitionToNextScene); //3:click monitor -> next scene
 
-        //ReactOnInput(0); //uncomment this line if first event needs to start defaultly
+        ReactOnInput(0); //uncomment this line if first event needs to start defaultly
 
         AddEvent(0, ShowPlayerUIMessage);   // 0: show message
         AddEvent(1, HidePlayerUIMessage);   // 1: keyDown -> hide message
-        AddEvent(2, HidePlayerUIMessage);   // 2: keyDown -> hide message
-        AddEvent(3, ShowPlayerUIMessage);   // 3: keyDown on exam paper -> show message
-        AddEvent(4, ShowPlayerUIMessage);   // 4: keyDown -> show message
+        AddEvent(2, ShowPlayerUIMessage);   // 2: select on exam paper -> show message
+        AddEvent(3, HidePlayerUIMessage);   // 3: keyDown -> hide message
+        AddEvent(4, ShowPlayerUIMessage);   // 4: select on computer monitor -> show message
         AddEvent(5, HidePlayerUIMessage);   // 5: keyDown -> hide message
-        AddEvent(6, ShowPlayerUIMessage);   // 6: keyDown on monitor -> show message
+        AddEvent(6, ShowPlayerUIMessage);   // 6: keyDown on wine bottle -> show message
         AddEvent(7, ShowPlayerUIMessage);   // 7: keyDown -> show message
-        AddEvent(8, HidePlayerUIMessage);   // 8: keyDown -> hide message
-        AddEvent(9, ShowPlayerUIMessage);   // 9: keyDown on wine bottle -> show message
-        AddEvent(10, ShowPlayerUIMessage);   // 10: keyDown -> show message
-        AddEvent(11, ShowPlayerUIMessage);   // 11: keyDown -> show message
-        AddEvent(12, ShowPlayerUIMessage);   // 12: keyDown -> show message
-        AddEvent(13, ShowPlayerUIMessage);   // 13: keyDown -> show message
-        AddEvent(14, TransitionToNextScene);   // 14: keyDown -> change scene
-
-        ReactOnInput(0);
+        AddEvent(8, ShowPlayerUIMessage);   // 8: keyDown -> show message
+        AddEvent(9, ShowPlayerUIMessage);   // 9: keyDown -> show message
+        AddEvent(10, TransitionToNextScene);   // 10: keyDown -> transition to next scene
     }
 }
