7c85e18f1d1828ac6c9994369da63edfd3822a94
rickywu0421 <rickywu0421@gmail.com>
2021-01-02 21:51:09 +0800

    update stage8

--- Assets/Scripts/MonoBehaviourVR/Stage8/BallFallsInWater.cs
+++ Assets/Scripts/MonoBehaviourVR/Stage8/BallFallsInWater.cs
@@ -8,6 +8,7 @@ public class BallFallsInWater : MonoBehaviour
     bool isFalling = false;
     [SerializeField]
     Stage_8_Manager stage_8_Manager;
+    AudioSource audioSource;
     void OnCollisionEnter(Collision collisionInfo)
     {
         if (collisionInfo.gameObject.name != "Rectangle073" 
@@ -22,5 +23,17 @@ public class BallFallsInWater : MonoBehaviour
     IEnumerator DelayThenPlaySFX() {
         yield return new WaitForSeconds(1.0f);
         stage_8_Manager.PlaySFX();
+        audioSource = stage_8_Manager.Effect_player;
+        StartCoroutine(GraduallyDecreaseSFX());
+    }
+
+    IEnumerator GraduallyDecreaseSFX() {
+        float factor = 0.1f;
+
+        const int V = 10;
+        for (int i = 0; i < V; i++) {
+            audioSource.volume -= factor;
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 }
